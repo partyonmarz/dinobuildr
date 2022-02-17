b0cc7b460e22b63988094501b81c485867d8d2a8
Patrick Jagla <partyonmarz@gmail.com>
2022-02-16 16:10:37 -0800

    Trying to update this to work with the build
    Just trying some random changes.  Do not know much about Bash but switched a few things up and am hoping I at least get past my first error which is
    
    "
    Starting the build!
    
    Downloading the manifest file and hash-checking it.
    
    https://raw.githubusercontent.com/mozilla/dinobuildr/master/production_manifest.json
    /var/tmp/dinobuildr/production_manifest.json
    /var/tmp/dinobuildr/production_manifest.json is 6260 bytes.
          6260 [100.00%]
    ***** DINOBUILDR IS BUILDING. RAWR. *****
    
    Building against the [master] branch and the production_manifest.json manifest
    
    Downloading: Check macOS Version
    /var/tmp/dinobuildr/macos-versioncheck.sh is 1409 bytes.
    The hash for /var/tmp/dinobuildr/macos-versioncheck.sh match the manifest file
    Executing: Check macOS Version
    *** You are running macOS 12.2.1, which meets the minimum requirements.
    
    Downloading: Download Wallpaper Image
    /var/tmp/dinobuildr/wallpaper-firefox-file.png is 118041 bytes.
    The hash for /var/tmp/dinobuildr/wallpaper-firefox-file.png match the manifest file
    File downloaded to: /var/tmp/dinobuildr/wallpaper-firefox-file.png
    
    Downloading: Configure Wallpaper
    /var/tmp/dinobuildr/wallpaper.sh is 4237 bytes.
    The hash for /var/tmp/dinobuildr/wallpaper.sh match the manifest file
    Executing: Configure Wallpaper
    *** Copying /var/tmp/dinobuildr/wallpaper-firefox-file.png to /Users/Shared/wallpaper-firefox-file.png
    *** 8e425e645e2d4295c9e196e2786578f594d9829494b0084cdd2336c1fd1a2682
    *** We're on Big Sur so we're going to try to use the Big Sur way to set the wallpaper.
    *** Traceback (most recent call last):
    ***   File "<string>", line 3, in <module>
    *** IOError: [Errno 2] No such file or directory: 'options.json'
    *** ERROR: This script should not be used in a dual-monitor environment.
    ********************************************************************
    HOUSTON WE HAVE A PROBLEM: Dinobuildr did not complete successfully.
    Please review the error and run the this build again."

--- resources/wallpaper.sh
+++ resources/wallpaper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-osascript <<E0D
+osascript <<EOF
 tell application "System Events"
 	tell every desktop
 		set picture to "/Users/Shared/wallpaper-firefox-file.png"
 	end tell
 end tell
-EOD
+EOF
 HASH = "5abff4bcbe3cbb607cdd92210cc803629b06cd83cb8ee6f532d2cc17646ed853"
-    exit 1
+    exit 0
